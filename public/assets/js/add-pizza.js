1ef4846a09ae3c3ac705b41a4b4391033c6f2314
quaylas <quayla@cxadvisors.com>
2021-03-21 13:44:29 -0600

    finished add-pizza functionality and connected to front-end

--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -57,8 +57,17 @@ const handlePizzaSubmit = event => {
     headers: {
       Accept: 'application/json',
       'Content-Type' : 'application/json'
-    }
+    },
+    body: JSON.stringify(formData)
   })
+  .then(response => response.json())
+  .then(postResponse => {
+    alert('Pizza created successfully!');
+    console.log(postResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 };
 
 $pizzaForm.addEventListener('submit', handlePizzaSubmit);
